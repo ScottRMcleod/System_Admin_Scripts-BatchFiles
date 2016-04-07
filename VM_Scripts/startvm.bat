a1e4789785751d54c7d86ddd3e67175f94729fd7
Scott Mcleod <smcleod_06@hotmail.com>
2016-04-07 12:51:18 +1000

    Create startvm.bat

---
+++ VM_Scripts/startvm.bat
@@ -0,0 +1,3 @@
+REM this starts up a vitural machine once the host machine has been started 
+REM you will need to first setup and enviroment variable on your machine to get this script working 
+vmrun start c:\users\smcleod\documents\virtual_machines\vmdc-ad01\vmdc-ad01.vmx

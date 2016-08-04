a2afd6668d9432d02974ae0682d184354f253277
Scott Mcleod <scott.mcleod@smcleodtech.com.au>
2016-08-04 21:59:28 +1000

    Update Join_to_domain.bat

--- BatchScripts/Join_to_domain.bat
+++ BatchScripts/Join_to_domain.bat
@@ -2,20 +2,23 @@ echo off
 echo Your are about to join this machine the the eraring energy network 
 echo Please Press the enter button to continue:
 pause
+REM Activates Windows 78 Client with Software Product Key
 echo Activating Windows 7
 cd C:
-slmgr.vbs -ipk FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4
+slmgr.vbs -ipk XXXXX-XXXXX-XXXXX-XXXXX-XXXXX
 slmgr.vbs -ato
 echo Joining Machine to AD....
-powershell add-computer -domain eraring-energy.com.au -cred eraring\adm_PCDomainJoin
+powershell add-computer -domain yourdomain.com.au -cred domain\domain_authenticatedUserAccount
 
 echo updating the Group Policie...
 pause
 
+REM forces the update of your Group Policy on the machine
 gpupdate /force 
 
 echo machine will now restart to Apply Computer Settings
 pause
 
+REM - restarrts machine with no timer
 shutdown -r -t 00
 
